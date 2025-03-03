632b0dc2717703c327168d67548de2f8a597a65b
MarioV27 <mario.119140179@student.itera.ac.id>
2025-03-03 09:27:28 +0700

    Update Dashboard

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -14,18 +14,18 @@ class Dashboard extends Controller
         $tglskrg = Carbon::now()->timezone('Asia/Jakarta');
         $tahun = $tglskrg->format('Y');
         $bulan = $tglskrg->format('m');
-        $hari = $tglskrg->format('d');
+        $bulan1 = (int) $tglskrg->format('m'); 
+        $bulan_sebelumnya = $bulan1 == 1 ? '12' : sprintf('%02d', $bulan1 - 1);
 
         $pendapatan_skrg=kas::where('nama','pemasukan'.$tahun."-".$bulan)->value('jumlah');
-        $pendapatan_kmrn=kas::where('nama','pemasukan'.$tahun."-".$bulan-1)->value('jumlah');
+        $pendapatan_kmrn=kas::where('nama','pemasukan'.$tahun."-".$bulan_sebelumnya)->value('jumlah');
         $pengeluaran_skrg=kas::where('nama','pengeluaran'.$tahun."-".$bulan)->value('jumlah');
-        $pengeluaran_kmrn=kas::where('nama','pengeluaran'.$tahun."-".$bulan-1)->value('jumlah');
+        $pengeluaran_kmrn=kas::where('nama','pengeluaran'.$tahun."-".$bulan_sebelumnya)->value('jumlah');
         $penyusutan_skrg=penyusutan::where('tanggal','like',$tahun.'-'.$bulan.'%')->sum('selisih');
         $kas=kas::where('nama','Balance')->value('jumlah');
         $piutang_masuk=piutang_pemasukan::sum('sisa');
         $piutang_keluar=piutang_pengeluaran::sum('sisa');
         $total_gaji=karyawan::sum('gaji');
-
         return view('welcome',[
             'title' => 'Dashboard-SIARAN',
             'pendapatan_skrg' => $pendapatan_skrg,
